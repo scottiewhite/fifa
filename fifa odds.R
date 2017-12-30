bd378b34f77f143ad9d49331017c6e5ed1b71d9b
scottiewhite <34718642+scottiewhite@users.noreply.github.com>
2017-12-30 09:31:03 -0800

    updating to include vigorish

--- fifa odds.R
+++ fifa odds.R
@@ -15,6 +15,14 @@ dbListTables(db)
 
 #Initialize the tables
 matchData <- dbReadTable(db, "Match")
+leagueData <- dbReadTable(db, "League")
+playerData <- dbReadTable(db, "Player")
+
+players <- matchData %>%
+  select(home_player_X1:away_player_11) %>%
+  na.omit() %>%
+  View()
+  
 
 #Pare down table and define results
 matchData <- matchData %>%
@@ -25,41 +33,87 @@ matchData <- matchData %>%
   mutate(home_win_odds = 1/LBH) %>%
   mutate(home_draw_odds = 1/LBD) %>%
   mutate(home_loss_odds = 1/LBA) %>%
+  mutate(vig = home_loss_odds + home_draw_odds + home_win_odds) %>%
+  mutate(no_vig_home_win_odds = home_win_odds/vig) %>%
+  mutate(no_vig_home_draw_odds = home_draw_odds/vig) %>%
+  mutate(no_vig_home_loss_odds = home_loss_odds/vig) %>%
   mutate(index = 1)
-  
-# Summary wins
-summary_wins <- matchData %>%
+
+# No Vigorish summary wins
+no_vig_summary_wins <- matchData %>%
+  group_by(no_vig_home_win_odds) %>%
+  summarize(losses = sum(home_loss), 
+            draws = sum(home_draw), 
+            wins = sum(home_win), 
+            games = sum(index))
+
+# No Vigorish Wins
+no_vig_summary_wins %>%
+  filter(games>25) %>%
+  ggplot(aes(x = no_vig_home_win_odds, y = wins/games, size = games)) + 
+  geom_point() + 
+  geom_abline(slope=1) + 
+  geom_smooth(method = "lm") +
+  xlab(label = "Pre-game implied home-team win probability") + 
+  ylab(label = "Actual Home-team Win %") +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent)
+
+# Vigorish summary wins
+vig_summary_wins <- matchData %>%
   group_by(home_win_odds) %>%
   summarize(losses = sum(home_loss), 
             draws = sum(home_draw), 
             wins = sum(home_win), 
             games = sum(index))
 
-# Wins
-summary_wins %>%
-  filter(games>50) %>%
-  ggplot(aes(x = home_win_odds, y = wins/games)) + 
+# Vigorish Wins
+vig_summary_wins %>%
+  filter(games>25) %>%
+  ggplot(aes(x = home_win_odds, y = wins/games, size = games)) + 
   geom_point() + 
   geom_abline(slope=1) + 
+  geom_smooth(method = "lm") +
   xlab(label = "Pre-game implied home-team win probability") + 
   ylab(label = "Actual Home-team Win %") +
   scale_x_continuous(labels = scales::percent) +
   scale_y_continuous(labels = scales::percent)
 
-
-# Summary draws
-summary_draws <- matchData %>%
+# Vigorish Summary draws
+vig_summary_draws <- matchData %>%
   group_by(home_draw_odds) %>%
   summarize(losses = sum(home_loss), 
             draws = sum(home_draw), 
             wins = sum(home_win), 
             games = sum(index))
 
-# Draws
-summary_draws %>%
+# Vigorish Draws
+vig_summary_draws %>%
   filter(games>25) %>%
-  ggplot(aes(x = home_draw_odds, y = draws/games)) + 
+  ggplot(aes(x = home_draw_odds, y = draws/games, size = games)) + 
   geom_point() + 
+  geom_abline(slope=1) + 
+  geom_smooth(method = "lm") +
+  xlab(label = "Pre-game implied home-team draw probability") + 
+  ylab(label = "Actual Home-team Draw %") +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent)
+
+
+# No Vigorish Summary draws
+no_vig_summary_draws <- matchData %>%
+  group_by(no_vig_home_draw_odds) %>%
+  summarize(losses = sum(home_loss), 
+            draws = sum(home_draw), 
+            wins = sum(home_win), 
+            games = sum(index))
+
+# No Vigorish Draws
+no_vig_summary_draws %>%
+  filter(games>25) %>%
+  ggplot(aes(x = no_vig_home_draw_odds, y = draws/games, size = games)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") +
   geom_abline(slope=1) + 
   xlab(label = "Pre-game implied home-team draw probability") + 
   ylab(label = "Actual Home-team Draw %") +
